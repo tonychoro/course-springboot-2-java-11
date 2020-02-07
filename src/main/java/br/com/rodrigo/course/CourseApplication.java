ba4f9c5535c7de7a0333e5ab2e9392492c262efe
Antonio Rodrigo <tonychoro@hotmail.com>
2020-02-07 01:42:43 -0300

    user entity and resource

--- src/main/java/br/com/rodrigo/course/CourseApplication.java
+++ src/main/java/br/com/rodrigo/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package br.com.rodrigo.demo;
+package br.com.rodrigo.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
